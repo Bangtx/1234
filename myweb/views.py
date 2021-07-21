26e73b8ba6b567057096543798c5473fc3fd270e
Bangtx <72857647+Bangtx@users.noreply.github.com>
2021-07-21 22:57:01 +0700

    Add files via upload

--- myweb/views.py
+++ myweb/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+class MyWeb:
+
+    def index(self, r):
+        return HttpResponse('ok')
