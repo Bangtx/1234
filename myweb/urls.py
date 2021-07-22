48687d7e839b7a68d6b048d4dab0a4cc58e4ab78
Bangtx <72857647+Bangtx@users.noreply.github.com>
2021-07-22 11:28:07 +0700

    Update urls.py

--- myweb/urls.py
+++ myweb/urls.py
@@ -4,5 +4,8 @@ from . views import myweb
 
 
 urlpatterns = [
-    path('', myweb().test, name='test'),
+    path('test/', myweb().test, name='test'),
+    path('', myweb().index, name='index'),
+    path('product/<int:id>', myweb().product, name='product'),
+    path('productdetails/<int:id>', myweb().product_details, name='product-details')
 ]
